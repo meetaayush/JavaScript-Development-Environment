2ef3cb216615f64b691af13518f6da9f89124292
meetaayush <meetaayush@gmail.com>
2019-04-27 03:20:50 +0530

    Refactored the code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import numeral from 'numeral';
+import "./index.css";
+import numeral from "numeral";
 
-const courseValue = numeral(1000).format('$0,0.00');
+const courseValue = numeral(1000).format("$0,0.00");
 console.log(`I would pay ${courseValue} for this awesome course`);
--- webpack.development.config.js
+++ webpack.development.config.js
@@ -1,21 +1,31 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  devtool: 'inline-source-map',
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Development',
-      template: './src/index.html'
+      title: "Development",
+      template: "./src/index.html"
     })
   ],
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'views'),
-    publicPath: '/'
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "views"),
+    publicPath: "/"
+  },
+  module: {
+    rules: [
+      { test: "/.js$/", exclude: "/node_modules/", loader: "babel-loader" },
+      {
+        test: /.css$/,
+        exclude: /node_modules/,
+        loaders: ["style-loader", "css-loader"]
+      }
+    ]
   }
 };
